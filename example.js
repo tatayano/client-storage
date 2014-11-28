0e38d75466ca33331c90bccbad4a97895077a9df
tatayano <tata.yano85@gmail.com>
2014-11-28 16:44:32 -0200

    Update example.js

--- example.js
+++ example.js
@@ -2,14 +2,11 @@ var cookie = new Cookie,
     local = new LocalStorage,
     session = new SessionStorage;
 
-cookie.setInfo('user1', {name: 'Glauco', sobrenome: 'Yano'}, 2 );
+cookie.setInfo('user1', {name: 'Glauco', lastname: 'Yano'}, 2 );
 var text = cookie.getInfo('user1');
-document.querySelector('div#div1').innerText = text.name + ' ' + text.sobrenome;
 
-local.setInfo('user2', {name: 'Jhonatan', sobrenome: 'Silva'}, 2);
+local.setInfo('user2', {name: 'Glauco', lastname: 'Yano'}, 2);
 var text = local.getInfo('user2');
-document.querySelector('div#div2').innerText = text.name + ' ' + text.sobrenome;
 
-session.setInfo('user3', {name: 'Fernando', sobrenome: 'Basilio'});
+session.setInfo('user3', {name: 'Glauco', lastname: 'Yano'});
 var text = session.getInfo('user3');
-document.querySelector('div#div3').innerText = text.name + ' ' + text.sobrenome;
